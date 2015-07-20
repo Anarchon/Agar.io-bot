060f0666be876e6726e0c3408481787390a4f5c7
Anarchon <christian.schweinegger@gmail.com>
2015-07-20 11:50:28 +0200

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -42,7 +42,7 @@ function getLatestCommit() {
                 window.jQuery("#" + prefix + "Dialog").show();
             }
 
-            window.jQuery.get('https://raw.githubusercontent.com/" + Account + "/Agar.io-bot/" + Branch + "/launcher.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
+            window.jQuery.get('https://raw.githubusercontent.com/' + Account + '/Agar.io-bot/' + Branch + '/launcher.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
                 var latestVersion = data.replace(/(\r\n|\n|\r)/gm, "");
                 latestVersion = latestVersion.substring(latestVersion.indexOf("// @version") + 11, latestVersion.indexOf("// @grant"));
 
