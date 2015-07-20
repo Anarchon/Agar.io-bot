4bfcf95667334ecc0f2a4e76e3feb83a0823910d
Anarchon <christian.schweinegger@gmail.com>
2015-07-20 14:27:07 +0200

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -19,46 +19,10 @@ Array.prototype.peek = function() {
 var Account = "Anarchon";
 var Branch = "beta";
 var sha = "efde0488cc2cc176db48dd23b28a20b90314352b";
-function getLatestCommit() {
-    window.jQuery.ajax({
-            url: "https://api.github.com/repos/" + Account + "/Agar.io-bot/git/refs/heads/" + Branch,
-            cache: false,
-            dataType: "jsonp"
-        }).done(function(data) {
-            console.dir(data["data"])
-            console.log("hmm: " + data["data"]["object"]["sha"]);
-            sha = data["data"]["object"]["sha"];
-    
-            function update(prefix, name, url) {
-                window.jQuery(document.body).prepend("<div id='" + prefix + "Dialog' style='position: absolute; left: 0px; right: 0px; top: 0px; bottom: 0px; z-index: 100; display: none;'>");
-                window.jQuery('#' + prefix + 'Dialog').append("<div id='" + prefix + "Message' style='width: 350px; background-color: #FFFFFF; margin: 100px auto; border-radius: 15px; padding: 5px 15px 5px 15px;'>");
-                window.jQuery('#' + prefix + 'Message').append("<h2>UPDATE TIME!!!</h2>");
-                window.jQuery('#' + prefix + 'Message').append("<p>Grab the update for: <a id='" + prefix + "Link' href='" + url + "' target=\"_blank\">" + name + "</a></p>");
-                window.jQuery('#' + prefix + 'Link').on('click', function() {
-                    window.jQuery("#" + prefix + "Dialog").hide();
-                    window.jQuery("#" + prefix + "Dialog").remove();
-                });
-                window.jQuery("#" + prefix + "Dialog").show();
-            }
 
-            window.jQuery.get('https://raw.githubusercontent.com/' + Account + '/Agar.io-bot/' + Branch + '/launcher.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
-                var latestVersion = data.replace(/(\r\n|\n|\r)/gm, "");
-                latestVersion = latestVersion.substring(latestVersion.indexOf("// @version") + 11, latestVersion.indexOf("// @grant"));
-
-                latestVersion = parseFloat(latestVersion + 0.0000);
-                var myVersion = parseFloat(aposLauncherVersion + 0.0000);
-
-                if (latestVersion > myVersion) {
-                    update("aposLauncher", "launcher.user.js", "https://github.com/" + Account + "/Agar.io-bot/blob/" + sha + "/launcher.user.js/");
-                }
-                console.log('Current launcher.user.js Version: ' + myVersion + " on Github: " + latestVersion);
-            });
-
-        }).fail(function() {});
-}
-getLatestCommit();
 
 console.log("Running Bot Launcher!");
+console.log("Bot Launcher:"+aposLauncherVersion);
 (function(d, e) {
 
     //UPDATE
