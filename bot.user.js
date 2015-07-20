6d16e6ac4716728a04020786f774b35355d9d4c3
Anarchon <christian.schweinegger@gmail.com>
2015-07-20 14:25:37 +0200

    Update bot.user.js
    Removed Updater

--- bot.user.js
+++ bot.user.js
@@ -20,51 +20,12 @@ var aposBotVersion = 3.554;
 Number.prototype.mod = function(n) {
     return ((this % n) + n) % n;
 };
-
+
 Array.prototype.peek = function() {
     return this[this.length - 1];
 };
 
 var sha = "efde0488cc2cc176db48dd23b28a20b90314352b";
-function getLatestCommit() {
-    window.jQuery.ajax({
-            url: "https://api.github.com/repos/" + Account +"/Agar.io-bot/git/refs/heads/" + Branch ,
-            cache: false,
-            dataType: "jsonp"
-        }).done(function(data) {
-            console.dir(data["data"])
-            console.log("hmm: " + data["data"]["object"]["sha"]);
-            sha = data["data"]["object"]["sha"];
-
-            function update(prefix, name, url) {
-                window.jQuery(document.body).prepend("<div id='" + prefix + "Dialog' style='position: absolute; left: 0px; right: 0px; top: 0px; bottom: 0px; z-index: 100; display: none;'>");
-                window.jQuery('#' + prefix + 'Dialog').append("<div id='" + prefix + "Message' style='width: 350px; background-color: #FFFFFF; margin: 100px auto; border-radius: 15px; padding: 5px 15px 5px 15px;'>");
-                window.jQuery('#' + prefix + 'Message').append("<h2>UPDATE TIME!!!</h2>");
-                window.jQuery('#' + prefix + 'Message').append("<p>Grab the update for: <a id='" + prefix + "Link' href='" + url + "' target=\"_blank\">" + name + "</a></p>");
-                window.jQuery('#' + prefix + 'Link').on('click', function() {
-                    window.jQuery("#" + prefix + "Dialog").hide();
-                    window.jQuery("#" + prefix + "Dialog").remove();
-                });
-                window.jQuery("#" + prefix + "Dialog").show();
-            }
-
-            window.jQuery.get('https://raw.githubusercontent.com/' + Account +'/Agar.io-bot/'+ Branch + '/bot.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
-                var latestVersion = data.replace(/(\r\n|\n|\r)/gm,"");
-                latestVersion = latestVersion.substring(latestVersion.indexOf("// @version")+11,latestVersion.indexOf("// @grant"));
-
-                latestVersion = parseFloat(latestVersion + 0.0000);
-                var myVersion = parseFloat(aposBotVersion + 0.0000); 
-                
-                if(latestVersion > myVersion)
-                {
-                    update("aposBot", "bot.user.js", "https://github.com/" + Account +"/Agar.io-bot/blob/" + sha + "/bot.user.js/");
-                }
-                console.log('Current bot.user.js Version: ' + myVersion + " on Github: " + latestVersion);
-            });
-
-        }).fail(function() {});
-}
-getLatestCommit();
 
 console.log("Running Apos Bot!");
 (function(f, g) {
